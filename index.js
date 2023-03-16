12a7d96540aec729ab5206e3a0625771032eec46
ConsolattaChepchirchir <consochep97@gmail.com>
2023-03-16 19:16:55 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide()
+{
+    let num1=2000;
+    let num2=100;
+    return num1/num2;
+}
+let square=()=>
+{
+    let x=2;
+    return x**2;
+}
+let add=(a,b)=>
+{
+    return a+b;
+
+}
